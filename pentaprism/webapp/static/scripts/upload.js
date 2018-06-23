4844a194fd4f927b1f85013667bc3cc7fafcd66e
Ben <compdude222@gmail.com>
2018-06-22 21:40:08 -0700

    Better Uploader

---
+++ pentaprism/webapp/static/scripts/upload.js
@@ -0,0 +1,33 @@
+function UploadFiles() {
+    $.ajax({
+        // Your server script to process the upload
+        url: '/images/',
+        type: 'POST',
+
+        // Form data
+        data: new FormData($('#fileform')[0]),
+
+        // Tell jQuery not to process data or worry about content-type
+        // You *must* include these options!
+        cache: false,
+        contentType: false,
+        processData: false,
+
+        // Custom XMLHttpRequest
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                // For handling the progress of the upload
+                myXhr.upload.addEventListener('progress', function (e) {
+                    if (e.lengthComputable) {
+                        $('progress').attr({
+                            value: e.loaded,
+                            max: e.total,
+                        });
+                    }
+                }, false);
+            }
+            return myXhr;
+        }
+    });
+}
