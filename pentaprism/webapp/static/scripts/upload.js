ea27acddf603ff8cbf176800198a0cb76be33f39
Ben <compdude222@gmail.com>
2018-06-26 17:53:37 -0700

    Better multi-uploader

--- pentaprism/webapp/static/scripts/upload.js
+++ pentaprism/webapp/static/scripts/upload.js
@@ -1,33 +1,78 @@
 function UploadFiles() {
-    $.ajax({
-        // Your server script to process the upload
-        url: '/images/',
-        type: 'POST',
+    files = $("#files")[0].files;
+    $("#progresses").empty();
+    for (var f = 0; f < files.length; f++) {
+        file = $("#files")[0].files[f];
+        pdiv = $("#progresses").append($("<div />"));
+        upload = function (img_file, pdiv) {
+            var plabel = $("<label />");
+            var pbar = $("<progress />");
+            plabel.text(img_file.name);
+            pdiv.append(plabel);
+            plabel.append(pbar);
 
-        // Form data
-        data: new FormData($('#fileform')[0]),
+            var fdata = new FormData();
+            fdata.append('', img_file);
+            
+            var pbar_progress = function (e) {
+                if (e.lengthComputable) {
+                    pbar.attr({
+                        value: e.loaded,
+                        max: e.total,
+                    });
+                }
+            };
 
-        // Tell jQuery not to process data or worry about content-type
-        // You *must* include these options!
-        cache: false,
-        contentType: false,
-        processData: false,
+            var pbar_done = function (data, s, j) {
+                if (data.saved.length == 1) {
+                    plabel.text(`${img_file.name} Done`);
+                }
+                else if (data.skipped.length == 1) {
+                    plabel.text(`${img_file.name} Skipped`);
+                }
+                else if (data.errored.length == 1) {
+                    plabel.text(`${img_file.name} ERROR`);
+                }
+                else {
+                    plabel.text(`${img_file.name} !!!`);
+                    console.log(j);
+                }
+            };
 
-        // Custom XMLHttpRequest
-        xhr: function () {
-            var myXhr = $.ajaxSettings.xhr();
-            if (myXhr.upload) {
-                // For handling the progress of the upload
-                myXhr.upload.addEventListener('progress', function (e) {
-                    if (e.lengthComputable) {
-                        $('progress').attr({
-                            value: e.loaded,
-                            max: e.total,
-                        });
+            var pbar_error = function (e) {
+                plabel.text(`${img_file.name} ABORTED`);
+            };
+            
+            $.ajax({
+                // Your server script to process the upload
+                url: '/images/',
+                type: 'POST',
+        
+                // Form data
+                data: fdata,
+        
+                // Tell jQuery not to process data or worry about content-type
+                // You *must* include these options!
+                cache: false,
+                contentType: false,
+                processData: false,
+                
+                success: pbar_done,
+                error: pbar_error,
+        
+                // Custom XMLHttpRequest
+                xhr: function () {
+                    var myXhr = new XMLHttpRequest(); //$.ajaxSettings.xhr();
+                    if (myXhr.upload) {
+                        myXhr.upload.addEventListener('progress', pbar_progress, false);
+                        // myXhr.upload.addEventListener("load", pbar_done);
+                        // myXhr.upload.addEventListener("error", pbar_error);
+                        // myXhr.upload.addEventListener("abort", pbar_error);
                     }
-                }, false);
-            }
-            return myXhr;
+                    return myXhr;
+                }
+            });
         }
-    });
+        upload(file, pdiv);
+    }
 }
