5a326684090d151be36534422ab44759f9cd7d6e
Ben <compdude222@gmail.com>
2019-07-04 00:47:48 -0700

    Using Axios and Vue

--- pentaprism/webapp/static/scripts/upload.js
+++ pentaprism/webapp/static/scripts/upload.js
@@ -1,82 +1,92 @@
-function UploadFiles() {
-    files = $("#files")[0].files;
-    $("#progresses").empty();
-    for (var f = 0; f < files.length; f++) {
-        file = $("#files")[0].files[f];
-        pdiv = $("<div />");
-        $("#progresses").append(pdiv);
-        upload = function (img_file, pdiv) {
-            var plabel = $("<label />");
-            var pbar = $("<progress />");
-            plabel.text(img_file.name);
-            pdiv.append(plabel);
-            plabel.append(pbar);
+const instance = axios.create({
+    baseURL: '/'
+});
 
-            var fdata = new FormData();
-            fdata.append('', img_file);
-
-            var pbar_progress = function (e) {
-                if (e.lengthComputable) {
-                    pbar.attr({
-                        value: e.loaded,
-                        max: e.total,
-                    });
-                }
-            };
-
-            var pbar_done = function (data, s, j) {
-                if (data.saved.length == 1) {
-                    var pimg = $("<img />");
-                    pdiv.append(pimg);
-                    $.get(`/images/${data.thumbnails[img_file.name]}/thumbnail/`, (data) => { pimg.attr("src", data) });
-                    plabel.text(`${img_file.name} Done`);
-                }
-                else if (data.skipped.length == 1) {
-                    plabel.text(`${img_file.name} Skipped`);
-                }
-                else if (data.errored.length == 1) {
-                    plabel.text(`${img_file.name} ERROR`);
-                }
-                else {
-                    plabel.text(`${img_file.name} !!!`);
-                    console.log(j);
-                }
-            };
-
-            var pbar_error = function (e) {
-                plabel.text(`${img_file.name} ABORTED`);
-            };
-
-            $.ajax({
-                // Your server script to process the upload
-                url: '/images/',
-                type: 'POST',
-
-                // Form data
-                data: fdata,
-
-                // Tell jQuery not to process data or worry about content-type
-                // You *must* include these options!
-                cache: false,
-                contentType: false,
-                processData: false,
-
-                success: pbar_done,
-                error: pbar_error,
-
-                // Custom XMLHttpRequest
-                xhr: function () {
-                    var myXhr = new XMLHttpRequest(); //$.ajaxSettings.xhr();
-                    if (myXhr.upload) {
-                        myXhr.upload.addEventListener('progress', pbar_progress, false);
-                        // myXhr.upload.addEventListener("load", pbar_done);
-                        // myXhr.upload.addEventListener("error", pbar_error);
-                        // myXhr.upload.addEventListener("abort", pbar_error);
-                    }
-                    return myXhr;
-                }
-            });
+Vue.component('file-upload', {
+    props: ['name'],
+    data: function () {
+        return {
+            max: 100,
+            value: 0,
+            thumb: "",
+            error: ""
         }
-        upload(file, pdiv);
+    },
+    template: `<div class="file">
+    <span>{{ name }}</span>
+    <progress v-bind:max="max" v-bind:value="value"/>
+    <img v-bind:src="thumb" />
+    <p>{{ error }}</p>
+</div>`
+});
+
+const progresses = new Vue({
+    el: '#progresses',
+    data: {
+        files: []
     }
+});
+
+function ThumbnailFill(idx, id) {
+    instance.get(`/images/${id}/thumbnail/`)
+        .then((response) => {
+            progresses.$children[idx].thumb = response.data;
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function UploadFiles() {
+    $("#upload_button").attr("disabled", true);
+    files = $("#files")[0].files;
+
+    requests = [];
+    name_map = {};
+
+    progresses.files.splice(0, progresses.files.length);
+
+    for (var f = 0; f < files.length; f++) {
+        let file = $("#files")[0].files[f];
+
+        let fdata = new FormData();
+        fdata.append('', file);
+
+        let this_idx = progresses.files.push({
+            name: file.name
+        }) - 1;
+
+        req = instance.post('images/', fdata, {
+            onUploadProgress: (progressEvent) => {
+                progresses.$children[this_idx].max = progressEvent.total;
+                progresses.$children[this_idx].value = progressEvent.loaded;
+            }
+        })
+            .then((response) => {
+                console.log(response);
+                if (response.data.saved.includes(file.name)) {
+                    progresses.$children[this_idx].error = "";
+                    ThumbnailFill(this_idx, response.data.thumbnails[file.name]);
+                } else if (response.data.skipped.includes(file.name)) {
+                    progresses.$children[this_idx].error = "Skipped";
+                    progresses.$children[this_idx].thumb = "";
+                } else if (response.data.errored.includes(file.name)) {
+                    progresses.$children[this_idx].error = "ERROR";
+                    progresses.$children[this_idx].thumb = "";
+                } else {
+                    progresses.$children[this_idx].error = "!!!";
+                    progresses.$children[this_idx].thumb = "";
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        requests.push(req);
+    }
+    axios.all(requests)
+        .then((result) => {
+            console.log(result);
+            $("#upload_button").attr("disabled", false);
+        });
 }
